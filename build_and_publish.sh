68cf656e45f1eb8912ed9c8e52cc88870d3ad8c8
em <eugen.mayer@kontextwork.de>
2022-09-07 09:44:54 +0200

    remove linux/arm/v7

--- build_and_publish.sh
+++ build_and_publish.sh
@@ -2,4 +2,4 @@
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 docker buildx create --use
 # build and push
-docker buildx build --push --build-arg OCAML_VERSION=$OCAML_VERSION --build-arg UNISON_VERSION=$UNISON_VERSION --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag eugenmayer/unison:$UNISON_VERSION-$OCAML_VERSION .
+docker buildx build --push --build-arg OCAML_VERSION=$OCAML_VERSION --build-arg UNISON_VERSION=$UNISON_VERSION --platform linux/arm64/v8,linux/amd64 --tag eugenmayer/unison:$UNISON_VERSION-$OCAML_VERSION .
