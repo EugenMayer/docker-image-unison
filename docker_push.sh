d46736c87d08e6957a4d18b3a55da5cdd044a1f3
em <eugen.mayer@kontextwork.de>
2022-09-07 08:13:23 +0200

    merge combine and push

--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "creating manifests for different arches"
+docker manifest create eugenmayer/unison:$UNISON_VERSION-$OCAML_VERSION eugenmayer/unison:$UNISON_VERSION-$OCAML_VERSION-AMD64 eugenmayer/unison:$UNISON_VERSION-$OCAML_VERSION-ARM64
+docker manifest push eugenmayer/unison:$UNISON_VERSION-$OCAML_VERSION
+
+echo "deploying on docker hub"
 docker push eugenmayer/unison
