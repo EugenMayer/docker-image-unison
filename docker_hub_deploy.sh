9afa6e9a87f1f909f9a5a8f042c6d07f84155f1c
em <eugen.mayer@kontextwork.de>
2022-09-07 08:20:09 +0200

    Refactor script, rename it to prepare ghcr.io pushed too

--- docker_hub_deploy.sh
+++ docker_hub_deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-echo "creating manifests for different arches"
-docker manifest create eugenmayer/unison:$UNISON_VERSION-$OCAML_VERSION eugenmayer/unison:$UNISON_VERSION-$OCAML_VERSION-AMD64 eugenmayer/unison:$UNISON_VERSION-$OCAML_VERSION-ARM64
-docker manifest push eugenmayer/unison:$UNISON_VERSION-$OCAML_VERSION
+echo "Deploying different arches on docker hub"
+docker push eugenmayer/unison:$UNISON_VERSION-$OCAML_VERSION-AMD64
+docker push eugenmayer/unison:$UNISON_VERSION-$OCAML_VERSION-ARM64
 
-echo "deploying on docker hub"
-docker push eugenmayer/unison
+echo "Creating manifests for different arches: AMD64 / ARM64"
+docker manifest create eugenmayer/unison:$UNISON_VERSION-$OCAML_VERSION eugenmayer/unison:$UNISON_VERSION-$OCAML_VERSION-AMD64 eugenmayer/unison:$UNISON_VERSION-$OCAML_VERSION-ARM64
+echo "Pushing manifest: eugenmayer/unison:$UNISON_VERSION-$OCAML_VERSION"
+docker manifest push eugenmayer/unison:$UNISON_VERSION-$OCAML_VERSION
